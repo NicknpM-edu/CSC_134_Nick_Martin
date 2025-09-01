ffb71616de080c11a5a3f1d2ccd2d0212c9d4f24
Nick Martin <martinb513@mail.sandhills.edu>
2025-09-01 10:51:53 -0400

    gogo

---
+++ sqrt.cpp
@@ -0,0 +1,19 @@
+//@author: Nick Martin
+//@date: 8/27/2025
+//@purpose: This program prints the square root of a number
+
+//iostream
+#include <iostream>
+
+//game start
+int main()
+{
+    int num1 = 25;
+    int result = num1/num1;
+
+    //use the std output for the console and print product
+    std::cout<< result. << std::endl;
+
+    //end method
+    return 0;
+}
