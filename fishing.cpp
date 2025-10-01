987b3ccd08a1481a6cebecded123f5372d018709
SANDHILLS\LH218-16 <jad.talbert@gmail.com>
2025-10-01 11:56:54 -0400

    fish?

---
+++ fishing.cpp
@@ -0,0 +1,37 @@
+//@author: Nick Martin
+//@date started: 10/1/2025
+//@last updated: 10/1/2025
+//@purpose: Fishing game
+
+//iostream
+#include <iostream>
+#include <string>
+using namespace std;
+
+//game start
+int main()
+{
+    int dollar = 0;
+    string quit;
+    quit = "no";
+    while (quit!="0"){//begin game loop
+        cout<<"fishing or shop? press 0 to quit"<<endl;
+        cin>>choice;
+
+        if (choice = "fishing"){//fishing game
+        
+
+        }
+
+
+
+
+
+
+    }
+
+    cout<<"you end with $"<<dollar;
+
+    //end method
+    return 0;
+}
