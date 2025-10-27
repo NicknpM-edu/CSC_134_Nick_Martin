3c7de7079bacbd06894149de4ee50edf1e7ecfe4
SANDHILLS\LH218-16 <jad.talbert@gmail.com>
2025-10-27 12:15:05 -0400

    dgffdgdgffdgfdg

---
+++ array_ten.cpp
@@ -0,0 +1,27 @@
+//@author: Nick Martin
+//@date: 10/27/2025
+//@purpose: This program takes a list of grades and prints the average
+
+//iostream
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+//game start
+int main()
+{
+    
+    int grades[10];
+    int give;
+    int total = 0;
+    for(int count = 0;count<10;count++){
+        cout<<"give ten grades ";
+        cin>>grades[count];
+        total += grades[count];
+    }
+    int average = total/10;
+    cout<<average;
+    //end method
+    return 0;
+}
--- fishing.cpp
+++ fishing.cpp
@@ -29,9 +29,14 @@ int main()
         while(choice == "1"){//fishing game
             int value = ((rand() % fish_skill) + 1);// the result is ranges from 1 to 5
             int size = ((rand() % 1) + 1);//size fish
+            if (bait_num){
+                bait_num -= 1; //take bait
+                fish_skill +=
 
+            }
             cout<<"You got a size "<<size<<" fish, you sell it for $"<<value<<endl;//fish output
             dollar += value;// add to cash
+
 
             cout<<"\nYou have $"<<dollar<<". press 1 to keep fishing, press 2 to go shopping"<<endl; //ask for more fish or end
             cin>>choice;
@@ -49,9 +54,9 @@ int main()
                 cout<<"You got a new fishing rod, your fishing level is now "<<fish_skill<<"."<<endl;
             }
             if (shopping == "bait" and dollar>= bait_cost){
-                fish_skill += 10;
                 dollar -= bait_cost;//REMEMBER TO LOWER CASH
                 rod_cost = (bait_cost * 7);
+                bait_num = 10;
                 cout<<"You got ten pieces of bait."<<endl;
             }
             else {//poor :(
