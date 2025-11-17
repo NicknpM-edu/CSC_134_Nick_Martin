9b2c0c22a6139283386be38078d1fb3826c41dad
SANDHILLS\LH218-16 <jad.talbert@gmail.com>
2025-11-17 12:01:47 -0500

    gggggggggggggggggggg

---
+++ function_void.cpp
@@ -0,0 +1,21 @@
+//@author: Nick Martin
+//@date: 11/17/2025
+//@purpose: This program runs a function
+
+//iostream
+#include <iostream>
+using namespace std;
+
+void displayWelcome(){
+    cout<<"============================"<<endl;
+    cout<<"C++ Function Demonstration"<<endl;
+    cout<<"============================"<<endl;
+    return;
+}
+//game start
+int main()
+{
+    displayWelcome();
+    //end method
+    return 0;
+}
